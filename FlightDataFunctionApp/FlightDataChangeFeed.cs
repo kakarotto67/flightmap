b968f3a1015df7b1c85521b73db19a4d5229bb12
Roman Vovk <arromanvk@gmail.com>
2019-07-27 20:00:23 +0300

    Modified FlightDataChangeFeed Azure Function to push SignalR notification to client whenever data is modified (added).

--- FlightDataFunctionApp/FlightDataChangeFeed.cs
+++ FlightDataFunctionApp/FlightDataChangeFeed.cs
@@ -1,27 +1,42 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 using Microsoft.Extensions.Logging;
 
 namespace FlightDataFunctionApp
 {
    /// <summary>
    /// This Azure Function is triggered whenever CosmosDB is modified
+   /// and pushes the input value via SignalR to client's method
    /// </summary>
    public static class FlightDataChangeFeed
    {
       [FunctionName("FlightDataChangeFeed")]
-      public static void Run([CosmosDBTrigger(
+      public static async Task RunAsync([CosmosDBTrigger(
             databaseName: "flightmap-db",
             collectionName: "flightmap-container",
             ConnectionStringSetting = "AzureCosmosDBConnection",
             LeaseCollectionName = "leases",
-            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> input, ILogger log)
+            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> input,
+         [SignalR(HubName = "flightdata")] IAsyncCollector<SignalRMessage> signalRMessages,
+         ILogger log)
       {
          if (input != null && input.Count > 0)
          {
             log.LogInformation("Documents modified " + input.Count);
-            log.LogInformation("First document Id " + input[0].Id);
+            //log.LogInformation("First document Id " + input[0].Id);
+
+            // Push SignalR messages to client's method newFlightData() with flight information
+            foreach (var flight in input)
+            {
+               await signalRMessages.AddAsync(new SignalRMessage
+               {
+                  Target = "newFlightData",
+                  Arguments = new[] { flight }
+               });
+            }
          }
       }
    }
